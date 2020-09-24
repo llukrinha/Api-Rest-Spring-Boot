ad89b9ae73817c7e5e2449f97d61e61cf8f0521b
llukrinha <llukrinha@gmail.com>
2020-09-24 15:45:56 -0300

    Api Test JunitProfilTest

---
+++ src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
@@ -0,0 +1,12 @@
+package br.com.alura.forum.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AutenticacaoControllerTest {
+
+    @Test
+    public void autenticar() {
+    }
+}
